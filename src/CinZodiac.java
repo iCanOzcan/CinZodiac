31d611d1a0419ec22d17a7867498006afd748013
iCanOzcan <123066429+iCanOzcan@users.noreply.github.com>
2023-03-30 11:00:56 +0300

    Update CinZodiac.java

--- src/CinZodiac.java
+++ src/CinZodiac.java
@@ -8,27 +8,27 @@ public class CinZodiac {
 
         if(year%12==0){
             System.out.println("Cin zodyagi burcunuz: Maymun");
-        } else if (year%12==1) {
+        }if (year%12==1) {
             System.out.println("Cin zodyagi burcunuz: Horoz");
-        }else if (year%12==2) {
+        }if (year%12==2) {
             System.out.println("Cin zodyagi burcunuz: Kopek");
-        }else if (year%12==3) {
+        }if (year%12==3) {
             System.out.println("Cin zodyagi burcunuz: Domuz");
-        }else if (year%12==4) {
+        }if (year%12==4) {
             System.out.println("Cin zodyagi burcunuz: Fare");
-        }else if (year%12==5) {
+        }if (year%12==5) {
             System.out.println("Cin zodyagi burcunuz: Okuz");
-        }else if (year%12==6) {
+        }if (year%12==6) {
             System.out.println("Cin zodyagi burcunuz: Kaplan");
-        }else if (year%12==7) {
+        }if (year%12==7) {
             System.out.println("Cin zodyagi burcunuz: Tavsan");
-        }else if (year%12==8) {
+        }if (year%12==8) {
             System.out.println("Cin zodyagi burcunuz: Ejderha");
-        }else if (year%12==9) {
+        }if (year%12==9) {
             System.out.println("Cin zodyagi burcunuz: Yılan");
-        }else if (year%12==10) {
+        }if (year%12==10) {
             System.out.println("Cin zodyagi burcunuz: At");
-        }else if (year%12==11) {
+        }if (year%12==11) {
             System.out.println("Cin zodyagi burcunuz: Koyun");
         }
     }
